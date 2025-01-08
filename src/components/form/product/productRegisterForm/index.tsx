b28c590344cc4455315ba42e16f995648e842626
이윤선 <108972919+yoonsun123@users.noreply.github.com>
2025-01-08 16:28:40 +0900

    Design :: 디자인 완성

---
+++ src/components/form/product/productRegisterForm/index.tsx
@@ -0,0 +1,213 @@
+import React, { useState } from "react";
+import { ProductData } from "@/type/product.type";
+import * as S from "./style";
+import { useNavigate } from "react-router-dom";
+import CategorySelector from "@/components/categorySelect";
+import { cityData } from "@/data/cityData";
+
+interface ProductFormProps {
+  initialData?: ProductData; // 수정 시 초기 데이터
+  onSubmit: (data: ProductData) => void;
+}
+
+const ProductForm: React.FC<ProductFormProps> = ({ initialData, onSubmit }) => {
+  const [title, setTitle] = useState(initialData?.title || "");
+  const [images, setImages] = useState<File[]>([]);
+  const [quantity, setQuantity] = useState(initialData?.quantity);
+  const [unitPrice, setUnitPrice] = useState(initialData?.unitPrice);
+  const [description, setDescription] = useState(
+    initialData?.description || ""
+  );
+  const [location1, setLocation1] = useState("");
+  const [location2, setLocation2] = useState("");
+  const [location3, setLocation3] = useState("");
+  const [location, setLocation] = useState(initialData?.location || "");
+  const [location2Options, setLocation2Options] = useState<string[]>([]); // location2 옵션을 관리
+  const [category, setCategory] = useState(initialData?.category || "어류"); // 단일 상태로 카테고리 관리
+  const [ownerId, setOwnerId] = useState(initialData?.ownerId || "");
+  const categories = ["패류", "어류", "해조류", "두족류", "갑각류", "기타"];
+  const navigate = useNavigate();
+
+  // location1, 2, 3 합치기
+  const updateLocation = () => {
+    setLocation(`${location1} ${location2} ${location3}`.trim());
+  };
+
+  // location1이 바뀔 때 location2 옵션 변경
+  const handleLocation1Change = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const newLocation1 = e.target.value;
+    setLocation1(newLocation1);
+
+    // 해당 도시의 구/군 목록을 가져오기
+    const selectedCity = cityData.find((city) => city.city === newLocation1);
+    if (selectedCity) {
+      setLocation2Options(selectedCity.districts);
+    } else {
+      setLocation2Options([]);
+    }
+
+    setLocation2(""); // 선택 초기화
+    updateLocation(); // location 업데이트
+  };
+
+  const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files) {
+      setImages(Array.from(e.target.files));
+    }
+  };
+
+  const triggerFileInput = () => {
+    document.getElementById("file-input")?.click(); // 숨겨진 input을 클릭하도록 유도
+  };
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    const formData: ProductData = {
+      title,
+      images,
+      quantity,
+      unitPrice,
+      description,
+      location,
+      category, // 수정된 category 상태 사용
+      ownerId,
+    };
+    onSubmit(formData);
+  };
+
+  const handleGoBack = () => {
+    navigate(-1); // 뒤로가기
+  };
+
+  return (
+    <S.AllWriteArea onSubmit={handleSubmit}>
+      <S.TopArea>
+        <div>
+        <S.FileInputWrapper onClick={triggerFileInput}>
+          {images.length > 0 ? (
+            <p>{images.length}개의 파일이 선택되었습니다</p>
+          ) : (
+            ""
+          )}
+          <S.FileInput
+            type="file"
+            id="file-input" // input component로 변경하기
+            multiple
+            accept="image/*"
+            onChange={handleImageChange}
+            required
+          />
+        </S.FileInputWrapper>
+        </div>
+        <S.RightTopArea>
+          <div>
+            <S.Ps style={{ marginBottom: "1vh" }}>제목</S.Ps>
+            <S.Inputs
+              type="text"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+              placeholder="계시물의 제목을 입력해주세요"
+              required
+            />
+          </div>
+          <S.MiddleOfTopArea>
+            <div>
+              <S.Ps style={{ marginBottom: "1vh" }}>단가</S.Ps>
+              <S.PriceInput
+                type="number"
+                value={unitPrice}
+                onChange={(e) => setUnitPrice(Number(e.target.value))}
+                placeholder="단가를 입력해주세요"
+                required
+              />
+            </div>
+            <div>
+              <S.Ps style={{ marginBottom: "1vh" }}>수량</S.Ps>
+              <S.QuantityInput
+                type="number"
+                value={quantity}
+                onChange={(e) => setQuantity(Number(e.target.value))}
+                min={1}
+                placeholder="수량을 입력해주세요"
+                required
+              />
+            </div>
+          </S.MiddleOfTopArea>
+          <div>
+            <S.Ps style={{ marginBottom: "1vh" }}>어종</S.Ps>
+            <S.SelectorsArea>
+              {categories.map((categoryOption, index) => (
+                <CategorySelector
+                  key={index}
+                  category={categoryOption}
+                  isSelected={category === categoryOption} // 선택된 카테고리와 비교
+                  onClick={() => setCategory(categoryOption)} // 카테고리 선택 시 업데이트
+                />
+              ))}
+            </S.SelectorsArea>
+          </div>
+        </S.RightTopArea>
+      </S.TopArea>
+      <S.AllBottoms>
+        <S.MiddleArea>
+          <S.Ps>장소</S.Ps>
+          <S.MiddleLocal>
+            <S.Local
+              value={location1}
+              onChange={handleLocation1Change} // location1 변경 시 호출
+            >
+              <option value="" disabled hidden>
+                시
+              </option>
+              {cityData.map((city) => (
+                <option key={city.city} value={city.city}>
+                  {city.city}
+                </option>
+              ))}
+            </S.Local>
+
+            <S.Local
+              value={location2}
+              onChange={(e) => {
+                setLocation2(e.target.value);
+                updateLocation();
+              }}
+            >
+              <option value="" disabled hidden>
+                군
+              </option>
+              {location2Options.map((option) => (
+                <option key={option} value={option}>
+                  {option}
+                </option>
+              ))}
+            </S.Local>
+            <S.Inputs
+              value={location3}
+              onChange={(e) => {
+                setLocation3(e.target.value);
+                updateLocation();
+              }}
+              placeholder="상세 장소를 입력해주세요"
+              required
+            />
+          </S.MiddleLocal>
+        </S.MiddleArea>
+        <div>
+          <S.Ps style={{ marginBottom: "1vh" }}>상세설명</S.Ps>
+          <S.DetailInfo
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+            required
+          />
+        </div>
+        <S.ButtonArea>
+            <S.SubmitBtn type="submit">등록</S.SubmitBtn>
+            <S.CencelBtn onClick={handleGoBack}>취소</S.CencelBtn>
+        </S.ButtonArea>
+      </S.AllBottoms>
+    </S.AllWriteArea>
+  );
+};
+
+export default ProductForm;
---
+++ src/components/form/product/productRegisterForm/style.ts
@@ -0,0 +1,181 @@
+import styled from "styled-components";
+
+export const AllWriteArea = styled.form`
+  display: flex;
+  flex-direction: column;
+  width: 60vw;
+`;
+export const TopArea = styled.div`
+  margin-top: 9vh;
+  display: flex;
+  width: 100%;
+  justify-content: space-between;
+`;
+export const RightTopArea = styled.div`
+  width: 30vw;
+  height: 50vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`;
+export const MiddleArea = styled.div`
+  margin-top: 3.5vh;
+  display: flex;
+  width: 100%;
+  justify-content: space-between;
+  align-items: center;
+`;
+export const MiddleLocal = styled.div`
+  width: 51.5vw;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+export const ButtonArea = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: center;
+  gap: 1.5vw;
+`;
+
+export const MiddleOfTopArea = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const FileInputWrapper = styled.div`
+  background-color: #d1d1d1;
+  width: 50vh;
+  height: 50vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  &:hover {
+    cursor: pointer;
+  }
+`;
+
+export const FileInput = styled.input`
+  display: none;
+`;
+
+export const DetailInfo = styled.textarea`
+  border: 2px solid #d1d1d1;
+  border-radius: 5px;
+  color: black;
+  font-size: 16px;
+  font-family: pretandard;
+  padding: 1.5vh 1vw;
+  resize: none;
+  width: 100%;
+  height: 40vh;
+  &::placeholder {
+    color: #d1d1d1;
+    font-size: 16px;
+  }
+  &:focus {
+    border: 2px solid #327DFF;
+    outline: none;
+  }
+  &.error {
+    border: 2px solid #ff3c3c;
+  }
+`;
+
+const InputBase = styled.input`
+  border: 2px solid #d1d1d1;
+  border-radius: 5px;
+  color: black;
+  font-size: 16px;
+  padding: 0 1vw;
+  height: 6.5vh;
+  &::placeholder {
+    color: #d1d1d1;
+    font-size: 16px;
+  }
+  -moz-appearance: textfield;
+  -webkit-appearance: none;
+  appearance: none;
+  &::-webkit-inner-spin-button,
+  &::-webkit-outer-spin-button {
+    -webkit-appearance: none;
+    margin: 0;
+  }
+  &:focus {
+    border: 2px solid #327DFF;
+    outline: none;
+  }
+  &.error {
+    border: 2px solid #ff3c3c;
+  }
+`;
+
+// Inputs들 글자수 제한 추가하기
+export const Inputs = styled(InputBase)`
+  width: 30vw;
+`;
+
+export const PriceInput = styled(InputBase)`
+  width: 17.5vw;
+`;
+
+export const QuantityInput = styled(InputBase)`
+  width: 11.5vw;
+`;
+
+export const Ps = styled.p`
+  font-size: 18px;
+  font-weight: bold;
+`;
+export const SelectorsArea = styled.div`
+height: 15vh;
+display: flex;
+  flex-wrap: wrap;
+  gap: 1vw;
+  justify-content: space-between;
+`;
+
+export const Local = styled.select`
+  border: 2px solid #d1d1d1;
+  border-radius: 5px;
+  width: 10vw;
+  height: 6.5vh;
+  padding-left: 1vw;
+  padding-right: 3vw;
+  font-size: 16px;
+  appearance: none;
+  background-image: url('src/assets/image/vector.svg');
+  background-repeat: no-repeat;
+  background-position: right 10px center;
+  background-size: 1.2vw;
+  &:hover{
+    cursor: pointer;
+  }
+`;
+export const AllBottoms = styled.div`
+  height: 68vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  margin-bottom: 20vh;
+`;
+export const SubmitBtn = styled.button`
+  width: 13.8vw;
+  height: 7.5vh;
+  background-color: #327DFF;
+  border: none;
+  border-radius: 5px;
+  color: #ffffff;
+  font-size: 20px;
+  font-weight: bold;
+`;
+export const CencelBtn = styled.button`
+  width: 13.8vw;
+  height: 7.5vh;
+  background-color: #ffffff;
+  border: 1px solid black;
+  border-radius: 5px;
+  color: #000000;
+  font-size: 20px;
+  font-weight: bold;
+`;
