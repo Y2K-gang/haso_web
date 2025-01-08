98218d5079b2a3d44b2202a59a3c5addca347397
이윤선 <108972919+yoonsun123@users.noreply.github.com>
2025-01-08 16:20:00 +0900

    Feat :: 디테일 화면 디자인 완성

---
+++ src/components/form/product/productDetailForm/style.ts
@@ -0,0 +1,54 @@
+import styled from "styled-components";
+
+export const AllAreaOfDetail = styled.div`
+display: flex;
+width: 70vw;
+justify-content: space-between;
+margin-top: 9vh;
+align-items: center;
+`
+export const InfoArea = styled.div`
+height: 56vh;
+width: 33vw;
+display: flex;
+flex-direction: column;
+justify-content: space-around;
+`
+export const InfoAreaInArea = styled.div`
+height: 14vh;
+display: flex;
+flex-direction: column;
+justify-content: space-between;
+`
+export const Groups = styled.div`
+gap: 2vh;
+display: flex;
+flex-direction: column;
+justify-content: space-around;
+`
+export const ImgArea = styled.div`
+width: 60vh;
+height: 60vh;
+border-radius: 10px;
+`
+
+export const Title = styled.span`
+font-size: 40px;
+font-weight: bold;
+`
+
+export const SemiBoldFonts = styled.span`
+font-size: 24px;
+font-weight: bold;
+`
+export const CategoryArea = styled.div`
+width: 8.5vw;
+height: 6.5vh;
+border: 2px solid #327DFF;
+border-radius: 5px;
+font-size: 20px;
+font-weight: bold;
+display: flex;
+align-items: center;
+justify-content: center;
+`
