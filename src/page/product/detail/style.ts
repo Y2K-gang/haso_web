e519c87bea0177804393d1ad341d5afd8df41925
이윤선 <108972919+yoonsun123@users.noreply.github.com>
2025-01-08 16:35:42 +0900

    Design :: 페이지 뷰 짜기 완성

---
+++ src/page/product/detail/style.ts
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+
+export const DetailBack = styled.div`
+display: flex;
+flex-direction: column;
+align-items: center;
+height: 82vh;
+justify-content: space-between;
+`
+export const EditBtn = styled.button`
+  width: 13.8vw;
+  height: 7.5vh;
+  background-color: #327DFF;
+  border: none;
+  border-radius: 5px;
+  color: #ffffff;
+  font-size: 20px;
+  font-weight: bold;
+`;
+export const DeleteBtn = styled.button`
+  width: 13.8vw;
+  height: 7.5vh;
+  background-color: #ffffff;
+  border: 1px solid black;
+  border-radius: 5px;
+  color: #000000;
+  font-size: 20px;
+  font-weight: bold;
+`;
--- src/page/product/register/style.ts
+++ src/page/product/register/style.ts
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-export const EditBack = styled.div`
+export const RegisterBack = styled.div`
 display: flex;
 justify-content: center;
 `
