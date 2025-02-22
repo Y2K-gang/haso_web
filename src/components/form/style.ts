58ef67b0302ef339846bdab004ba53b2671a3088
이윤선 <108972919+yoonsun123@users.noreply.github.com>
2025-02-22 22:58:25 +0900

    Fix :: form style 길이 단위 원상복구

--- src/components/form/style.ts
+++ src/components/form/style.ts
@@ -1,11 +1,12 @@
 import styled from "styled-components";
 
 export const Form = styled.form`
-    width: 787px;
+    width: 35vw;
+    //height: 55vh;
     padding: 1.563rem;
-    border: 0.5px solid;
+    border: 0.5vw solid;
     z-index: 1;
-    gap: 20px;
+    gap: 1.2vw;
     border-radius: ${({ theme }) => theme.borderRadius.large};
     background-color: ${({ theme }) => theme.colors.white};
     border-color: ${({ theme }) => theme.colors.primary[200]};
