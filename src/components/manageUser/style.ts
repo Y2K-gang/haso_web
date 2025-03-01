0752d87caede82ef9fcd724b5e7bf206367cd5ad
이윤선 <108972919+yoonsun123@users.noreply.github.com>
2025-03-02 01:21:10 +0900

    Feat :: 메인이랑 코드 맞추기

--- src/components/manageUser/style.ts
+++ src/components/manageUser/style.ts
@@ -1,14 +1,14 @@
 import styled from "styled-components";
 
 export const ManageUserBack = styled.div`
-  width: 90vw;
-  height: 13vh;
+  width: 1180px;
+  height: 100px;
   border-radius: 10px;
   background-color: ${({ theme }) => theme.colors.gray[400]};
   display: flex;
   justify-content: center;
   align-items: center;
-  margin-bottom: 4vh;
+  margin-bottom: 32px;
 
   &:hover {
     background-color: ${({ theme }) => theme.colors.gray[500]};
@@ -16,7 +16,7 @@ export const ManageUserBack = styled.div`
   }
 `;
 export const ManageUserContents = styled.div`
-  width: 1220px;
+  width: 1100px;
   display: flex;
   justify-content: space-between;
   align-items: center;
@@ -31,5 +31,5 @@ export const ManageUserContentsArea = styled.div`
 `;
 
 export const ManageUserArea = styled(ManageUserContentsArea)`
-  width: 11vw;
+  width: 157px;
 `;
